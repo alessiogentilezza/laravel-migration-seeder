620322b48013531060a4ba440741c77949d6c5a3
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-05-12 17:59:36 +0200

    poco poco style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,13 @@
 <div class="col-3">
     <div class="card m-3">
         <ul class="list-group list-group-flush">
-            <li class="list-group-item"> {{ $train->Orario_di_partenza }}</li>
+            <li class="list-group-item"> Azienda: {{ $train->Azienda }}</li>
+            <li class="list-group-item"> Stazione di partenza: {{ $train->Stazione_di_partenza }}</li>
+            <li class="list-group-item"> Stazione di arrivo: {{ $train->Stazione_di_arrivo }}</li>
+            <li class="list-group-item"> Orario di partenza: {{ $train->Orario_di_partenza }}</li>
+            <li class="list-group-item"> Orario di arrivo: {{ $train->Orario_di_arrivo }}</li>
+            <li class="list-group-item"> Codice Treno: {{ $train->Codice_treno }}</li>
+            <li class="list-group-item"> Numero Carrozze: {{ $train->Numero_carrozze }}</li>
         </ul>
     </div>
 </div>
