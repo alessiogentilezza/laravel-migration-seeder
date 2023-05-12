765f8793bb5652fb250cb53a35b4b9cc24ec87b9
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-05-12 18:06:50 +0200

    finito ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,18 @@
 @section('title', 'Home page')
 
 @section('content')
-<p>questi sono gli unici 2 treni in partezza oggi!</p>
+<h3 class="m-3">Questi sono gli unici 2 treni in partezza oggi!</h3>
 @foreach ($trains as $train)
 <div class="col-3">
     <div class="card m-3">
         <ul class="list-group list-group-flush">
-            <li class="list-group-item"> Azienda: {{ $train->Azienda }}</li>
-            <li class="list-group-item"> Stazione di partenza: {{ $train->Stazione_di_partenza }}</li>
-            <li class="list-group-item"> Stazione di arrivo: {{ $train->Stazione_di_arrivo }}</li>
-            <li class="list-group-item"> Orario di partenza: {{ $train->Orario_di_partenza }}</li>
-            <li class="list-group-item"> Orario di arrivo: {{ $train->Orario_di_arrivo }}</li>
-            <li class="list-group-item"> Codice Treno: {{ $train->Codice_treno }}</li>
-            <li class="list-group-item"> Numero Carrozze: {{ $train->Numero_carrozze }}</li>
+            <li class="list-group-item"> <strong>Azienda: </strong>  {{ $train->Azienda }}</li>
+            <li class="list-group-item"> <strong>Stazione di partenza: </strong> {{ $train->Stazione_di_partenza }}</li>
+            <li class="list-group-item"> <strong>Stazione di arrivo: </strong>{{ $train->Stazione_di_arrivo }}</li>
+            <li class="list-group-item"> <strong>Orario di partenza: </strong>{{ $train->Orario_di_partenza }}</li>
+            <li class="list-group-item"> <strong>Orario di arrivo: </strong> {{ $train->Orario_di_arrivo }}</li>
+            <li class="list-group-item"> <strong>Codice Treno: </strong>{{ $train->Codice_treno }}</li>
+            <li class="list-group-item"> <strong>Numero Carrozze: </strong> {{ $train->Numero_carrozze }}</li>
         </ul>
     </div>
 </div>
